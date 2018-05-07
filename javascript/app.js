9c5efa61f6d4af2aaff355969a5bf788167cd7eb
Han Ban <hannah15198@gmail.com>
2018-05-06 18:11:20 -0700

    organized into functions and user can search, new button will appear and gif will appear on click of new button- yay!

--- javascript/app.js
+++ javascript/app.js
@@ -1,3 +1,35 @@
+// add source attr
+// add data-state = animate
+// add width = 200px
+
+    var results;
+    // create image function**
+    // create image tag in html
+    var image = $("<img>");
+    var stillURL;
+    var animateURL;
+    var userInput;
+    var newButton = $("<button>");
+
+function imageTagAttr() {
+    // add attr to image tag
+    image.attr("src", stillURL);
+    image.attr("data-state", "still");
+    image.attr("width", "200px");
+    image.attr("class", "gif");
+    image.attr("data-still", stillURL);
+    image.attr("data-animate", animateURL);
+}
+
+function addButton() {
+    // add attributes to button
+    newButton.attr("data-animal", userInput);
+    newButton.attr("class", "image-button");
+
+    // put html in new button
+    newButton.html(userInput);
+}
+
 // on click button
 $("body").on("click", ".image-button", function () {
 
@@ -17,29 +49,17 @@ $("body").on("click", ".image-button", function () {
         console.log(queryURL)
         console.log(response)
         // find image url in JSON
-        var results = response.data;
+        results = response.data;
 
         // create image function**
         // create image tag in html
-        var image = $("<img>");
-        var stillURL = results.images.fixed_height_still.url;
-        var animateURL = results.image_original_url;
-
-
-        // add source attr
-        // add data-state = animate
-        // add width = 200px
-        function imageTagAttr() {
-        image.attr("src", stillURL);
-        image.attr("data-state", "still");
-        image.attr("width", "200px");
-        image.attr("class", "gif");
-        image.attr("data-still", stillURL);
-        image.attr("data-animate", animateURL);
-        }
+        image = $("<img>");
+        stillURL = results.images.fixed_height_still.url;
+        animateURL = results.image_original_url;
 
         // call imageTagAttr function to add attributes to image tag
-        imageTagAttr();
+        console.log (imageTagAttr());
+
         // body.on("click", ".gif", function())
         // on click gif
         image.on("click", function () {
@@ -68,16 +88,18 @@ $("body").on("click", ".image-button", function () {
 
 // on click 
 $("#search-button").on("click", function () {
-    // add class image-button
-    var newButton = $("<img>");
+    userInput = $("#userInput").val();
+    // create new button by calling addButton function
+    addButton();
+
+    // create new image by calling imageTagAttr function
     imageTagAttr();
 
     // user input will be data-animal
-    var userInput = $("#userInput");
     image.attr("data-animal", userInput);
 
     // append to div of buttons
-    $("#imagesDiv").prepend(image);
+    $("#imagesDiv").prepend(newButton);
 
 })
 
